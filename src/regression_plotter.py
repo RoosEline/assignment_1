eb44377a4dab48e12c8c2a9786153feee1c9d032
mnataliagf <152299241+mnataliagf@users.noreply.github.com>
2023-12-04 11:57:16 +0100

    Update regression_plotter.py

--- src/regression_plotter.py
+++ src/regression_plotter.py
@@ -3,3 +3,6 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 #class RegressionPlotter:
+
+#simple, 3D, 2D per feature the last one you take the feature and the corresponding data and
+#b is intercept and already at the weights so you need the position of b and which weight you are at 
