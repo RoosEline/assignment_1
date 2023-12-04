303ba61fdc71dc07cbc22d083348f41e96b4e291
mnataliagf <152299241+mnataliagf@users.noreply.github.com>
2023-12-04 11:50:56 +0100

    Update multiple_linear_regression.py

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 class MultipleLinearRegression:
-    def __init__ (self, default_intercept = 0, default_coefficients = None):
+    def __init__ (self, default_intercept = 0, default_coefficients = None, w):
         self.intercept = default_intercept
         self.coefficients = default_coefficients
 
     def getWeights()
-        xt = np.matrix.transpose(x)
-        w = np.matmul((np.matmul(np.linalg.inv(np.matmul(xt, x)), xt), y)
         return w
     
     def train(self, x, y): 
-        getWeights()
+        xt = np.matrix.transpose(x)
+        w = np.matmul((np.matmul(np.linalg.inv(np.matmul(xt, x)), xt), y)
+        self.weights = w
+        #change x to add the 1s
+        #comment that the data is expected in a specific format
 
     def predict(self, x, w):
         y = np.matmul(x, w)
